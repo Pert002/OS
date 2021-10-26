94193526ee9050a3f58c8c483e64be1de96efbcd
Pert002 <78302184+Pert002@users.noreply.github.com>
2021-10-27 00:03:28 +0300

    Add files via upload

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,162 +1,142 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
 #include <thread>
-#include <string>
+#include <vector>
+#include <unistd.h>
 
 using namespace std;
-
-
-
-void matrix_thread(vector<vector<double>> matrix, double (*convolution)[3], int line, int columns, double* resul)
-{
-    //double res = 0.0;
-    //double buffer[3][3];
-    for (int J = 0; J < columns - 2; J++) {
-        double res = 0.0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                res += matrix[line + i][J] * convolution[i][j];
-                cout << matrix[line + i][J] * convolution[i][j] << " ";
-            }
-            cout << "\n\n\n\n";
+ 
+ 
+void matrix_thread(int numberOfStr, int n, int m, int convolutionSize, double *convolution, double *matrix, double* results) {
+ 
+    double matrix2[n][m];
+    double convolution2[convolutionSize][convolutionSize];
+    double mresults[n - 2];
+    int q = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            matrix2[i][j] = matrix[q++];
         }
-        cout << "--------------------------------------------------\n";
-        resul[J] = res;
+    }
+ 
+    q = 0;
+    for (int i = 0; i < convolutionSize; i++) {
+        for (int j = 0; j < convolutionSize; j++) {
+            convolution2[i][j] = convolution[q++];
+        }
+    }
+ 
+    for(int i = 0; i < m - convolutionSize + 1; i++) {
+        double res = 0;
+        int a = 0, b = 0;
+        for(int j = numberOfStr; j < numberOfStr + convolutionSize; j++) {
+            for(int k = i; k < i + convolutionSize; k++) {
+                res += matrix2[j][k] * convolution2[a][b];
+                b++;
+            }
+            b = 0;
+            a++;
+        }
+        mresults[i] = res;
+    }
+ 
+    for (int i = 0; i < n - 2; i++) {
+        results[numberOfStr * (n - convolutionSize + 1) + i] = mresults[i];
     }
 }
-
-
-
-
-
-
-
+ 
+bool notCenter(int j, int n, int m) {                                                      
+    return (j >= 0 && j < m) || (j % m == 0) || ((j + 1) % m == 0) || (j < n * m && j > n * m - m);
+}
+ 
+ 
 int main(int argc, char* argv[]) {
-    int lines, columns, k;
-    cout << "k is \n";
-    cin >> k;
-    int ck = k;
-
-    int thread_amount = atoi(argv[1]);
-
-    cout << "Enter matrix's size:\n";
-    cin >> lines >> columns;
-
-    vector<vector<double>> matrix(lines, vector<double> (columns));
-    vector<vector<double>> matrix2(lines, vector<double> (columns));
-
-    cout << "Enter matrix:\n";
-    for (int i = 0; i < lines; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            cin >> matrix[i][j];
-            matrix2[i][j] = matrix[i][j];
-        }
+ 
+    if (argc != 2) {
+        cout << "Не достаточное кол-во аргументов" << endl                                                                                   ;
+        return 1;
+    } 
+    int K;
+    int threads_amount = atoi(argv[1]);
+ 
+    cout << "K:\n";
+    cin >> K;
+ 
+    if(!threads_amount || (threads_amount < 0 && threads_amount != -1))  {
+        cout << "Incorrect amount of threads" << endl;
+        return 1;
     }
-
-    double convolution[3][3];
-    cout << "Enter convolution matrix:\n";
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cin >> convolution[i][j];
-        }
+ 
+    double *convolution = new double[9];
+    cout << "Convolution matrix elements: " << endl;
+    for(int i = 0; i < 9; i++) {
+        cin >> convolution[i];
+    }                                                                                           
+ 
+    int n, m;
+    cout << "Matrix size:\n";
+    cin >> n >> m;
+    double *matrix = new double[n*m];
+    cout << "Matrix elements: " << endl;
+    for(int i = 0; i < n*m; i++) {
+        cin >> matrix[i];
+    } 
+ 
+    thread convolutionThreads[n - 2];
+ 
+    double *convolutionResult = new double[(n - 2) * (m - 2)];
+    for(int i = 0; i < (n - 2) * (m - 2); i++) {
+        convolutionResult[i] = 0;
     }
-
-    //double buffer[3][3];
-    //vector<vector<double>> result(lines - 2, vector<double>(columns - 2, 0));
-    vector<thread> threads_matrix(lines - 2);
-
-    while(k--) {
-        double *resul = new double[columns - 2];
-        vector<vector<double>> result(lines - 2, vector<double>(columns - 2, 0));
-        if (thread_amount == -1 || thread_amount >= lines - 2) {
-            for (int i = 0; i < lines - 2; i++) {
-                threads_matrix[i] = thread(matrix_thread, matrix, convolution, i, columns, resul);
+ 
+    while(K > 0) {
+ 
+        if(threads_amount == -1 || threads_amount >= n - 2) {
+            for(int i = 0; i < n - 2; i++) {
+                convolutionThreads[i] = thread(matrix_thread, i, n, m, 3, convolution, matrix, convolutionResult);
             }
-            for (int i = 0; i < lines - 2; i++) {
-                threads_matrix[i].join();
+            for(int i = 0; i < n - 2; i++) {
+                convolutionThreads[i].join();
             }
-            for (int i = 0; i < lines - 2; i++) {
-                for (int j = 0; j < columns - 2; j++) {
-                    result[i][j] = resul[j];
-                    //cout << "\n" << result[i][j] << "\n";
+        } 
+        else  {
+            for(int i = 0; i < n - 2; i++) {
+                if(i >= threads_amount && i != 0) {
+                    convolutionThreads[i - threads_amount].join();
                 }
+                convolutionThreads[i] = thread(matrix_thread, i, n, m, 3, convolution, matrix, convolutionResult);
+            }
+            for(int i = n - 3 - threads_amount + 1; i < n - 2; i++) {
+                convolutionThreads[i].join();
             }
         }
-
-        else {
-            for (int i = 0; i < lines - 2; i++) {
-                if(i >= thread_amount && i != 0) {
-                        threads_matrix[i - thread_amount].join();
-                }
-                threads_matrix[i] = thread(matrix_thread, matrix, convolution, i, columns, resul);
-            }
-            for (int i = lines - thread_amount - 2; i < lines - 2; i++) {
-                threads_matrix[i].join();
-            }
-            for (int i = 0; i < lines - 2; i++) {
-                for (int j = 0; j < columns - 2; j++) {
-                    //cout << resul[j];
-                    result[i][j] = resul[j];
-                    //cout << "\n" << result[i][j] << "\n";
-                }
+ 
+ 
+ 
+        int q = 0;
+        for (int j = 0; j < n * m; j++) {
+            if (notCenter(j, n, m)) { 
+                matrix[j] = 0;
+            } else  {
+                matrix[j] = convolutionResult[q];
+                q++;
             }
         }
-        matrix = result;
-        if (k != 0) {
-        lines = lines - 2;
-        columns = columns - 2;
-        }
+        K--;
     }
-    cout << "Result:\n";
-    for (int i = 0; i < lines - 2; i++)
-    {
-        for (int j = 0; j < columns - 2; j++)
-        {
-            cout << matrix[i][j] << " ";
+ 
+ 
+    cout << "Convolution result: " << endl;
+    for (int i = 0; i < (n * m); i++) {
+        if (!notCenter(i, n, m)) { 
+        cout << matrix[i] << " ";
         }
-        cout << endl;
+        if ((i + 1) % (m) == 0) cout << endl; 
     }
-    return 0;
-    //delete[] resul;
+ 
+    delete[] matrix;    
+    delete[] convolution;
+    delete[] convolutionResult;
+ 
 }
-
-//------------------------------------------------------------------------
-
-// First matrix (5x5)
-// 1 1 1 0 0 0 1 1 1 0 0 0 1 1 1 0 0 1 1 0 0 1 1 0 0
-//
-// 1 1 1 0 0 
-// 0 1 1 1 0
-// 0 0 1 1 1
-// 0 0 1 1 0
-// 0 1 1 0 0
-//
-// Result for k = 1:
-// 4 3 4
-// 2 4 3
-// 2 3 4
-
-//------------------------------------------------------------------------
-
-// Second matrix (6x6)
-// 1 1 1 0 0 1 0 1 1 1 0 1 0 0 1 1 1 0 0 0 1 1 0 1 0 1 1 0 0 0 0 1 1 0 0 1
-//
-// 1 1 1 0 0 1
-// 0 1 1 1 0 1
-// 0 0 1 1 1 0
-// 0 0 1 1 0 1
-// 0 1 1 0 0 0
-// 0 1 1 0 0 1
-
-//------------------------------------------------------------------------
-
-// Convolution matrix
-// 1 0 1 0 1 0 1 0 1
-//
-// 1 0 1
-// 0 1 0
-// 1 0 1
